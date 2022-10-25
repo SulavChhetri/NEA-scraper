20ff6f40e5f6cf03e9884eb839fae2731e660e64
Xulav <sulavthapachhetri@gmail.com>
2022-10-25 12:14:10 +0545

    prototype

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="src",
+    version="1.0.0",
+    description="This is a scraper of the energy details and iex details that are present in the nepal electricity authority website.",
+    author="Sulav",
+    packages=['src'],
+    install_requrires=[]
+    )
---
+++ src/main.py
@@ -0,0 +1,67 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import time
+import schedule
+from pathlib import Path
+import os
+ROOT_DIR = Path(__file__).parent.parent
+
+file_path = os.path.join(ROOT_DIR, 'files')
+
+
+def hourly_scrape():
+    schedule.every().hour.do(scrape_and_insert_product)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def daily_scrape():
+    schedule.every().hour.do(scrape_and_insert_product)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def scrape(url):
+    try:
+        r = requests.get(url).text
+        soup = BeautifulSoup(r, "html.parser")
+        scraped_data_html = soup.find_all('ul', class_='pl-1 text-primary')
+        energy_details_html = scraped_data_html[0]
+        iex_html = scraped_data_html[1]
+        energy_details = energy_details_html.find_all('h6')
+        iex = iex_html.find_all('h6')
+        enegry_details_list = [item.text for item in energy_details]
+        iex_list = [item.text for item in iex]
+        return enegry_details_list, iex_list
+    except:
+        hourly_scrape()
+
+
+def scrape_and_insert_product():
+    url = 'https://www.nea.org.np/'
+    nested_scraped_details = scrape(url)
+    if nested_scraped_details != None:
+        scrape_into_csv(nested_scraped_details[0], nested_scraped_details[1])
+
+
+def scrape_into_csv(energy_details, iex):
+    with open(os.path.join(file_path, "energydetails.csv"), 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Energy Demand", "Unit of Electricity"])
+        for item in energy_details:
+            energy_details_items = item.split(" – ")
+            writer.writerow([energy_details_items[0], energy_details_items[1]])
+
+    with open(os.path.join(file_path, "iex.csv"), 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Energy details', 'Content'])
+        for item in iex:
+            iex_items = item.split(' – ')
+            writer.writerow([iex_items[0], iex_items[1]])
+
+
+if __name__ == "__main__":
+    daily_scrape()
