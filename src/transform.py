9f8dce5803323de326c5f941f3a8f8e5d3c46bf7
Xulav <sulavthapachhetri@gmail.com>
2022-10-30 17:57:34 +0545

    clean

--- src/transform.py
+++ src/transform.py
@@ -1,3 +1,4 @@
+from calendar import c
 import pandas as pd
 import csv
 import os
@@ -7,10 +8,33 @@ ROOT_DIR = Path(__file__).parent.parent
 file_path_orginal = os.path.join(ROOT_DIR,'files','original')
 file_path_transformed = os.path.join(ROOT_DIR,'files','transformed')
 
+def iex_to_csv():
+    iex_details = pd.read_csv(os.path.join(file_path_orginal,'iex.csv'))
+    iex_header_dict = {'DATE :':'DATE'}
+    columns_list_split = iex_details.columns.values[1:5]
+    for column in columns_list_split:
+        for i in range(len(iex_details[column])):
+            item_split = iex_details[column][i].split()
+            item_number = item_split[0]
+            item_unit = ''.join(item_split[1:])
+            iex_details[column][i]=float(item_number)
+        iex_header_dict[column] = column+'('+item_unit+')'
+    
+    for column in iex_details.columns.values[5:]:
+        for i in range(len(iex_details[column])):
+            item_split_unit = iex_details[column][i].split(') ')
+            item_unit = item_split_unit[-1]
+            item_number_list = item_split_unit[0].split('(')
+            item_number_list= item_number_list[1].split('. ')
+            iex_details[column][i] = float(item_number_list[1])
+        iex_header_dict[column] = column+'[in '+item_number_list[0]+' '+item_unit+']'
+    iex_details.rename(columns =iex_header_dict,inplace=True)
+    iex_details.to_csv(os.path.join(file_path_transformed,'transformed_iex_details.csv'),index=False)
+
 def main():
     energy_details = pd.read_csv(os.path.join(file_path_orginal,'energydetails.csv'))
-    iex_details = pd.read_csv(os.path.join(file_path_orginal,'iex.csv'))
-    item_header_dict = {}
+    
+    energy_header_dict = {}
 
     for column in (energy_details.columns):
         for i in range(len(energy_details[column])):
@@ -18,7 +42,10 @@ def main():
             item_unit = item[1]
             energy_details[column][i] = int(item[0])
 
-        item_header_dict[column]= column+'('+item_unit+')'
-        energy_details.rename(columns =item_header_dict,inplace=True)
-        energy_details.to_csv(os.path.join(file_path_transformed,'transformed_energy_details.csv'),index=False)
+        energy_header_dict[column]= column+'('+item_unit+')'
+    energy_details.rename(columns =energy_header_dict,inplace=True)
+    energy_details.to_csv(os.path.join(file_path_transformed,'transformed_energy_details.csv'),index=False)
+
+    iex_to_csv()
+    
 main()
